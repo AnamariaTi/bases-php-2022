1b5dd08b01691f3227bfc0220b4d5dc30257e5a6
AnamariaTi <tirdeaana@outlook.fr>
2022-10-05 11:48:35 +0200

    le premier pag php

---
+++ 01-hello-world.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="description" content="PHP">
+    <meta name="keyword " content="Ana-Maria">
+
+    <title>Hello world </title>
+</head>
+<body>
+    <?php
+ echo "<h1>Hello World <h1>";
+// comentaire sur une ligne 
+// on peut mettre des balises php n'importe ou dans une page html
+
+
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
