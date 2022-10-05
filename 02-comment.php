73d164879f6be499ec4041f763090ac79bb7f041
AnamariaTi <tirdeaana@outlook.fr>
2022-10-05 12:35:18 +0200

    le 2eme page php

---
+++ 02-comment.php
@@ -0,0 +1,19 @@
+<?php 
+//commentaire sur une ligne
+
+# un autre maniére de faire un commentaire sur une ligne --c'est jammais termine 
+
+/*
+  commentaire sure plusieures ligne
+
+*/
+?>
+<p>commentaire html</p>
+<!-- 
+    ceci est un commentaire html , il sera envoyé au navigateur !
+--> 
+<?php
+// bonne pratique , ne  pas fermer pas la balise php quand ce n'est pas utile
+
+//affichage  avec echo , le ; est la fin de l'instruction 
+echo "<p> Temps Unix de la page :" ;  echo time(); // le tmp actuell de server depuis le 01/01/1970 
